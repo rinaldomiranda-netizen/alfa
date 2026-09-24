120d6cc850befb9a615a94d4d5f4cbce468d16c3
rinaldomiranda-netizen <rinaldomiranda@hotmail.com>
2026-09-24 09:29:48 -0300

    feat(atendimento): add Alpha module package

---
+++ modules/atendimento/__init__.py
@@ -0,0 +1,9 @@
+"""Módulo RMD Atendimento para o ALPHA.
+
+O módulo usa o protocolo já existente em ``core.atendimento`` como
+núcleo funcional, mantendo a implementação fora do Alpha Core.
+"""
+
+from .modulo import AtendimentoModule
+
+__all__ = ["AtendimentoModule"]
